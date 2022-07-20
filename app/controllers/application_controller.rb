bd73b29d862447662704bda6426b51c867e3c651
Dev1000 <javaimagination@gmail.com>
2022-07-20 14:22:56 -0500

    Add Dashboard page for Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # Redirect after successfully login
+  def after_sign_in_path_for(resource_or_scope)
+    users_dashboard_path(resource_or_scope)
+  end
+
   # Redirect to login page after sign out
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
