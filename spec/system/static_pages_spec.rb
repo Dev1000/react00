bd87dd1f43e47c7a1d1b42aa636dc6538b933fb6
Dev1000 <javaimagination@gmail.com>
2022-07-20 17:45:39 -0500

    Add static pages system tests

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -2,19 +2,35 @@
 
 require 'rails_helper'
 
-RSpec.describe '', type: :feature do
+RSpec.describe '', type: :system do
   describe 'index page' do
-
-    it 'shows the right content' do
-      visit root_path
-      sleep 2
-      expect(page).to have_content('Pages')
+    before :each do
+      User.create(email: 'user@example.com', password: 'password')
     end
 
-    it 'show the login process' do
+    it 'show the sign up process' do
       visit root_path
-      click_in 'Sign In'
+      click_link 'Sign Up'
+      fill_in 'Email', with: 'javaimagination@gmail.com'
+      fill_in 'Password', with: '123456'
+      fill_in 'Password confirmation', with: '123456'
+      sleep(1)
+      click_button 'Sign up'
+      sleep(1)
+      click_link 'Sign out'
+
+      expect(page).to have_content 'Log in'
     end
 
+    it 'shows the sign in process' do
+      visit '/users/sign_in'
+
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+      sleep(1)
+
+      expect(page).to have_content 'Users#dashboard'
+    end
   end
 end
